963f6c404c93c31a7da0f258eb62e59bd1a42e78
Miguel Ryan Dantas de Freitas <73431866+athavus@users.noreply.github.com>
2021-03-30 15:45:43 -0300

    Update conversor.js

--- Aula 1/Conversor de Moedas/conversor.js
+++ Aula 1/Conversor de Moedas/conversor.js
@@ -1,8 +1,5 @@
 var valorEmDolar = parseFloat(prompt("Qual o valor em dolar que você quer converter?"))
 
-var valorEmReal = valorEmDolar * 5.5
+var valorEmReal = parseFloat(valorEmDolar * 5.5).toFixed(2)
 
-valorEmReal = parseFloat(valorEmReal)
-var valorEmDecimal = valorEmReal.toFixed(2)
-
-alert(`O valor em dólares é $${valorEmDolar} e em reais é R$${valorEmDecimal}.`)
+alert(`O valor em dólares é $${valorEmDolar} e em reais é R$${valorEmReal}.`)
