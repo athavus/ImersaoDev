5782379c4f9aede099ba4397a52f988aa2bd43d9
Miguel Ryan Dantas de Freitas <73431866+athavus@users.noreply.github.com>
2021-03-30 18:40:04 -0300

    Create testewrite.js

---
+++ Aula 2/testewrite.js
@@ -0,0 +1,5 @@
+var nomeDoJogoDaRafa = "League of Legends"
+document.writeln(`Jogo da Rafa: ${nomeDoJogoDaRafa}`)
+
+var nomeDoJogoDoPaulo = "God Of War"
+document.writeln(`<br>Jogo do Paulo: ${nomeDoJogoDoPaulo}`)
